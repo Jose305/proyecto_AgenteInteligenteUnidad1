06c6dc1acbed2cae93e0c51a93f72e80b604f6a3
Jose305 <jaruiz17@espe.edu.ec>
2022-06-10 08:35:58 -0500

    Version 1

---
+++ proyecto_Unidad1.py
@@ -0,0 +1,13 @@
+""" 
+    Universidad de las Fuerzas Armadas ESPE Sede Santo Domingo de los Colorados
+    Departamento Ciencias de la Computación
+    Ingeniería en Tecnologías de la Información
+    
+    Autores: José Ruiz
+             Brandon Perengueza
+    Materia: Inteligencia Artificial
+
+    Programación del Agente Inteligente
+    Agente: Agrobot E-Series cosechador de fresas
+
+"""
