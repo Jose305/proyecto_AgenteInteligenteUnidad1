06469ff6e13f31884c5189c1b5dc42b5ce5d3889
Jose305 <jaruiz17@espe.edu.ec>
2022-06-10 08:54:55 -0500

    Version 5

--- proyecto_Unidad1.py
+++ proyecto_Unidad1.py
@@ -129,3 +129,81 @@ def Zona_3():
     print("Medición del rendimiento: " + str(costo))
 
 #******************************************************************************************************************
+#Se define la función que pertence a la zona 4
+def Zona_4():
+    """ Se inicializa la zona_4
+        Para cada localización su objetivo es tener el estado en 0 
+        Se indica que 0 es la fruta aun verde y 1 que la fruta esta totalmente madura lista para cosechar
+        Se inicializa en el costo 0
+    """
+    zona_4 = {'D': '0'}
+    costo = 0
+    #Nombre de la zona
+    print("\nZona 4 Otongo - Reproducción de Fresas \n")
+    #Variable de la zona 4 que permitira ingresar por consola al usuario
+    localizacion_D = input("Digite si las fresas de la zona estan verdes (0) si las fresas estan maduras (1) : ") 
+    #Objetivo alcanzar del zona 4
+    print("Objetivo en alcanzar: " + str(zona_4))
+    """ Se condiciona la localizacion que tomara el valor del usuario y ejecutara la acción determinada 
+        para el robot si realiza una accion aumentara el costo caso contrario se mantendra con su
+        valor inicial
+    """
+    if localizacion_D == '1':
+            #La zona se encuentra cosechada
+            print("La zona 4 se encuentra en estado de cosecha.")
+            #Aumenta el costo del agrobot
+            zona_4['D'] = '0'
+            costo += 1 #Imprime el mensaje de exito junto con el nuevo costo
+            print("La zona 4 ya ha sido cesechada con exito.")
+            print("Costo actual: " + str(costo))
+            
+    else:
+        #La zona de cultivo se encuentra cosechada. Por lo tanto no se realiza ninguna acción
+        print("La zona 4 se encuentra en estado de crecimiento de las fresas.")
+        print("Costo actual: " "("+ str(costo)+")")
+    #Se imprimira el resultado de la zona de cultivo seleccionado, alcanzo el objetivo con el costo.    
+    print("Estado de la zona 4: ")
+    print(zona_4)
+    print("Medición del rendimiento: " + str(costo))
+
+#******************************************************************************************************************
+
+#Se define la función que pertence a la zona 5
+def Zona_5():
+    """ Se inicializa la zona_5
+        Para cada localización su objetivo es tener el estado en 0 
+        Se indica que 0 es la fruta aun verde y 1 que la fruta esta totalmente madura lista para cosechar
+        Se inicializa en el costo 0
+    """
+    zona_5 = {'E': '0'}
+    costo = 0
+    #Nombre de la zona
+    print("\nZona 5 Naranjos - Reproducción de Fresas \n")
+    #Variable de la zona 5 que permitira ingresar por consola al usuario
+    localizacion_E = input("Digite si las fresas de la zona estan verdes (0) si las fresas estan maduras (1) : ") 
+    #Objetivo alcanzar del zona 5
+    print("Objetivo en alcanzar: " + str(zona_5))
+    """ Se condiciona la localizacion que tomara el valor del usuario y ejecutara la acción determinada 
+        para el robot si realiza una accion aumentara el costo caso contrario se mantendra con su
+        valor inicial
+    """
+    if localizacion_E == '1':
+            #La zona se encuentra cosechada
+            print("La zona 5 se encuentra en estado de cosecha.")
+            #Aumenta el costo del agrobot
+            zona_5['E'] = '0'
+            costo += 1 #Imprime el mensaje de exito junto con el nuevo costo 
+            print("La zona 5 ya ha sido cesechada con exito.")
+            print("Costo actual: " + str(costo))
+            
+    else:
+        #La zona de cultivo se encuentra cosechada. Por lo tanto no se realiza ninguna acción
+        print("La zona 5 se encuentra en estado de crecimiento de las fresas.")
+        print("Costo actual: " "("+ str(costo)+")")
+    #Se imprimira el resultado de la zona de cultivo seleccionado, alcanzo el objetivo con el costo.    
+    print("Estado de la zona 5: ")
+    print(zona_5)
+    print("Medición del rendimiento: " + str(costo))
+
+#******************************************************************************************************************
+    
