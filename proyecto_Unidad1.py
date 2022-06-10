1d7fc02dfa45138e57a7b486542f4fc1fe6127a6
Jose305 <jaruiz17@espe.edu.ec>
2022-06-10 08:50:42 -0500

    Version 2

--- proyecto_Unidad1.py
+++ proyecto_Unidad1.py
@@ -11,3 +11,6 @@
     Agente: Agrobot E-Series cosechador de fresas
 
 """
+""" En el siguiente programa se codifica un agente inteligente con el objetivo
+    de automatizar las cosechas de las fresas de manera optima 
+"""
